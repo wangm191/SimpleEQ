eb30be46768fe3ca591ac1511fa4f38131e2aeb5
Myles Wang <formyleswang@gmail.com>
2024-09-08 13:03:47 -0500

    added gain labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -387,6 +387,27 @@ void ResponseCurveComponent::resized()
         
         g.drawFittedText(str, rec, juce::Justification::centred, 1);
     }
+    
+    for ( auto gDb : gain)
+    {
+        auto y = jmap( gDb, -24.f, 24.f, float(bottom), float(top));
+        String str;
+        
+        if ( gDb > 0 )
+            str << "+";
+        str << gDb;
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> rec;
+        rec.setSize(textWidth, getHeight());
+        rec.setX(getWidth() - textWidth);
+        rec.setCentre(rec.getCentreX(), y);
+        
+        g.setColour(gDb == 0.f ? Colours::greenyellow : Colours::lightgrey);
+        
+        g.drawFittedText(str, rec, juce::Justification::centred, 1);
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
